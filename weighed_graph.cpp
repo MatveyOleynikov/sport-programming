c892966d2ead82d181b9bbeafc0e35aa329cd487
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2022-10-01 23:48:30 +0300

    Update weighed_graph.cpp

--- weighed_graph.cpp
+++ weighed_graph.cpp
@@ -40,4 +40,60 @@ struct weighed_graph{
         }
         return o;
     }
+
+    vector<long long> djkstra(long long start){
+        vector<long long> ans(n + 1);
+        vector<long long> pr(n + 1);
+        for (long long i = 1; i <= n; i++) {
+            ans[i] = inf64;
+            pr[i] = -1;   //Значение, обозначающее что из этой вершины возвращаться некуда
+        }
+
+        ans[start] = 0;
+
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> q;
+
+        q.push({0, start});
+
+        while (!q.empty()) {
+            pair<long long, long long> c = q.top();
+            q.pop();
+
+            long long dst = c.first, v = c.second;
+
+            if (ans[v] < dst) {
+                continue;
+            }
+
+            for (pair<long, long> e: g[v]) {
+                int u = e.first, len_vu = e.second;
+
+                int n_dst = dst + len_vu;
+                if (n_dst < ans[u]) {
+                    ans[u] = n_dst;
+                    pr[u] = v;
+                    q.push({n_dst, u});
+                }
+            }
+        }
+
+        return ans;
+        /*vector<long long> path;
+
+        long long cur = end;
+        path.push_back(cur);
+
+        while (pr[cur] != -1) {
+            cur = pr[cur];
+            path.push_back(cur);
+        }
+
+        reverse(path.begin(), path.end());
+
+        cout << "Shortest path between vertices " << start + 1 << " and " << end + 1 << " is: " << endl;
+
+        for (long long v: path) {
+            cout << v + 1 << ", ";
+        }*/ /// если нужен сам путь
+    }
 };
