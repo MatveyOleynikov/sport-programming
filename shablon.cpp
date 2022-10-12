6430a2d83a60a8efff0bb17c2e91973edcaf988d
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2022-10-12 23:26:15 +0300

    Update shablon.cpp

--- shablon.cpp
+++ shablon.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 #ifdef MATVEY
-#define dbg(x) do { cout << #x << "=" << x << endl;} while(0)
+#define _dbg(x) do { cout << #x << "=" << x << "; "; } while (0)
+#define _name(name, _1, _2, _3, _4, N, ...) name ## N
+#define _dbg1(x) _dbg(x)
+#define _dbg2(x, ...) _dbg(x); _dbg1(__VA_ARGS__)
+#define _dbg3(x, ...) _dbg(x); _dbg2(__VA_ARGS__)
+#define _dbg4(x, ...) _dbg(x); _dbg3(__VA_ARGS__)
+#define dbg(...) do { cout << __LINE__ << ": "; _name(_dbg, __VA_ARGS__, 4, 3, 2, 1, 0)(__VA_ARGS__); } while (0)
 #else
-#define dbg(x)
+#define dbg(...)
 #endif
 
 #define X first
