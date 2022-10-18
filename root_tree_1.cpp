0d85f56e89b13d914a1dfabe5c272c82472acda8
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2022-10-18 16:59:29 +0300

    Update root_tree_1.cpp

--- root_tree_1.cpp
+++ root_tree_1.cpp
@@ -1,6 +1,24 @@
 struct root_tree_1{
     long long n;
     vector<vector<long long>> g;
+    
+    friend ostream& operator<<(ostream& o, const graph& cur)
+    {
+        o << "Vertexes: " << cur.n << "\n";
+        o << "Edges: " << cur.n - 1 << "\n";
+
+        for (long long u = 1; u <= cur.n; ++u){
+            o << u << ":";
+            o << '(';
+            for (auto v: cur.g[u]){
+                o << v;
+                o << ',';
+            }
+            o << ')';
+            o << '\n';
+        }
+        return o;
+    }
 
     friend istream& operator>>(istream& o, root_tree_1& cur)
     {
