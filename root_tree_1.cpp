a545429700ceb2622c16f96e1674b8559beca92c
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2022-10-18 15:49:31 +0300

    Create root_tree_1.cpp

---
+++ root_tree_1.cpp
@@ -0,0 +1,17 @@
+struct root_tree_1{
+    long long n;
+    vector<vector<long long>> g;
+
+    friend istream& operator>>(istream& o, graph& cur)
+    {
+        o >> cur.n;
+        cur.g.resize(cur.n + 1);
+
+        for (long long i = 2; i <= cur.n; ++i){
+            long long parent;
+            cin >> parent;
+            g[parent].push_back(i);
+        }
+        return o;
+    }
+};
