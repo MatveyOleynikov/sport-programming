9429e08bdaf641186311831db251b1f504e5dbc5
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2022-10-19 23:51:04 +0300

    Create perebor_C.cpp

---
+++ perebor_C.cpp
@@ -0,0 +1,21 @@
+struct perebor_C{
+    static int calc(vi& a){
+        return 0;
+    }
+
+    static int C(int n, int k, vi& a){
+        if (n == k){
+            return calc(a);
+        }
+
+        int res = 0;
+
+        a.push_back(0);
+        res = max(res, C(n, k + 1, a));
+        a.pop_back();
+
+        a.push_back(1);
+        res = max(res, C(n, k + 1, a));
+        a.pop_back();
+    }
+};
