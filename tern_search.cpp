a98af302bce11777d5fefe0a9b3a507871af078e
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2022-10-25 23:28:36 +0300

    Update tern_search.cpp

--- tern_search.cpp
+++ tern_search.cpp
@@ -1,9 +1,14 @@
-int l = 0, r = n, EPS = 1; // входные данные
-while (r - l > EPS) {
-   int m1 = l + (r - l) / 3,
-      m2 = r - (r - l) / 3;
-   if (f(m1, a, n, q) < f(m2, a, n, q))
-      l = m1;
-   else
-      r = m2;
-} ///это зациклится надо поменять EPS и взять ответ от 3 значений
+    int l = 0, r = n, EPS = 2; // входные данные
+    while (r - l > EPS) {
+        dbg(l, r);
+       int m1 = l + (r - l) / 3,
+          m2 = r - (r - l) / 3;
+       if (check_ans(m1, a, n, q) < check_ans(m2, a, n, q))
+          l = m1;
+       else
+          r = m2;
+    }
+
+    int k = l;
+    if (check_ans(l, a, n, q) < check_ans(r, a, n, q)) k = r;
+    if (check_ans(l, a, n, q) < check_ans(l + 1, a, n, q)) k = r;
