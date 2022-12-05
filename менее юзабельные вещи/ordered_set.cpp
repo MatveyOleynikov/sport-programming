2d8c6d686768b7544ed38631f83dfef8ba53a215
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2022-12-05 14:35:55 +0300

    Create ordered_set.cpp

---
+++ менее юзабельные вещи/ordered_set.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef long long ll;
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+//order_of_key (k) : Number of items strictly smaller than k .
+//find_by_order(k) : K-th element in a set (counting from zero).
+//less(greater) for set and less_equal(greater_equal) for multiset
