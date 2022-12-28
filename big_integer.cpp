dd79e0b024e878e1c5cd2092154d6c9d087ca756
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2022-12-28 17:58:47 +0300

    Update big_integer.cpp

--- big_integer.cpp
+++ big_integer.cpp
@@ -1,10 +1,17 @@
 struct big_integer{
     const int glob_sz = 30;
+    const int osn = 1e9;
 
 	int sz;
 	vi vals;
 
 	big_integer(){sz=0; vals.resize(glob_sz);}
+
+	big_integer(int n){
+	    if (n > 1e9) exit(1);
+	    sz=1; vals.resize(glob_sz);
+	    vals[0] = n;
+    }
 
 	friend ostream& operator<<(ostream& o, const big_integer& cur){
 		if(!cur.sz){
@@ -36,9 +43,9 @@ struct big_integer{
         big_integer res;
         for(int i = 0; i < mx; i++){
             res.vals[i] += this->vals[i] + other.vals[i];
-            while (res.vals[i] >= 1000000000){
+            while (res.vals[i] >= osn){
                 res.vals[i + 1] += 1;
-                res.vals[i] -= 1000000000;
+                res.vals[i] -= osn;
             }
         }
 
@@ -50,9 +57,55 @@ struct big_integer{
         return res;
     }
 
+    big_integer operator *(const int& b){
+        big_integer cpy;
+        cpy.sz = this->sz;
+        cpy.vals = this->vals;
+        int carry = 0;
+        for (size_t i = 0; i < cpy.vals.size() || carry; ++i) {
+            long long cur = carry + cpy.vals[i] * 1ll * b;
+            cpy.vals[i] = INT (cur % osn);
+            carry = INT (cur / osn);
+        }
+
+        if (cpy.vals[cpy.sz]){
+            cpy.sz++;
+        }
+        return cpy;
+    }
+
+    big_integer operator /(const int& b){
+        dbg(b);
+        big_integer cpy;
+        cpy.sz = this->sz;
+        cpy.vals = this->vals;
+        int carry = 0;
+        for (int i=(int)cpy.vals.size()-1; i>=0; --i) {
+            long long cur = cpy.vals[i] + carry * 1ll * osn;
+            cpy.vals[i] = INT (cur / b);
+            carry = INT (cur % b);
+        }
+
+        cpy.sz--;
+
+        if (cpy.vals[cpy.sz]){
+            cpy.sz++;
+        }
+        return cpy;
+    }
+
     big_integer* operator =(const big_integer other){
         this->sz = other.sz;
         this->vals = other.vals;
+        return this;
+    }
+
+    big_integer* operator =(const int other){
+        if (other > 1e9){
+            exit(1);
+        }
+        this->sz = 1;
+        this->vals[0] = other;
         return this;
     }
 };
