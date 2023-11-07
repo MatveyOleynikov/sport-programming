1cb370c6ac969c98b755cc81a2243dc67e86bbb7
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2023-11-07 13:57:34 +0300

    Update big_integer.cpp

--- big_integer.cpp
+++ big_integer.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define int long long
+using vi = vector<int>;
+#define INT(X) static_cast<int>(X)
+
 struct big_integer{
     const int glob_sz = 5;
     const int osn = 1e9;
@@ -37,8 +47,6 @@ struct big_integer{
  
     big_integer operator +(const big_integer& other) const{
         int mx = max(this->sz, other.sz);
- 
-        //dbg(mx);
  
         big_integer res;
         for(int i = 0; i < mx; i++){
@@ -89,7 +97,6 @@ struct big_integer{
     }
  
     big_integer operator /(const int& b){
-        dbg(b);
         big_integer cpy;
         cpy.sz = this->sz;
         cpy.vals = this->vals;
@@ -122,4 +129,21 @@ struct big_integer{
         this->vals[0] = other;
         return this;
     }
+    
+    static big_integer make_big_integer(string s){
+        big_integer result(0);
+        for (int i = 0; i < s.size(); ++i){
+            result = result * 10;
+            result = result + (s[i] - '0');
+        }
+        return result;
+    }
 };
+
+signed main() {
+    big_integer bg = big_integer::make_big_integer("1234567432145678643467435425675435");
+    cout << bg;
+    return 0;
+}
+/*
+*/
