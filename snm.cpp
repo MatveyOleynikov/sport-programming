62e063c8a79b8f6ccbe463bf688c2bd930b2137a
matveyoleynikov <30356831+MatveyOleynikov@users.noreply.github.com>
2024-10-15 22:30:25 +0300

    Update snm.cpp

--- snm.cpp
+++ snm.cpp
@@ -1,38 +1,38 @@
-struct snm{
-    long long n;
-    vector<long long> clr;
-    vector<vector<long long>> obr_clr;
+struct snm {
+    int n;
+    vector<int> clr;
+    vector<vector<int>> obr_clr;
 
-    void join(long long color1, long long color2){
-        if (color1 == color2){
+    void join(int color1, int color2) {
+        if (color1 == color2) {
             return;
         }
-       
-        if (obr_clr[color1].size() < obr_clr[color2].size()){
+
+        if (obr_clr[color1].size() < obr_clr[color2].size()) {
             swap(color1, color2);
         }
 
-        for (auto elem: obr_clr[color2]){
+        for (auto elem : obr_clr[color2]) {
             obr_clr[color1].push_back(elem);
             clr[elem] = color1;
         }
     }
 
-    void join_elems(int u, int v){
+    void join_elems(int u, int v) {
         join(clr[u], clr[v]);
     }
 
-    bool equal_snm(int u, int v){
+    bool equal_snm(int u, int v) {
         return clr[u] == clr[v];
     }
 
-    snm(long long n){
+    snm(long long n) {
         this->n = n;
 
         clr.resize(n);
         obr_clr.resize(n);
 
-        for (int i = 0; i < n; ++i){
+        for (int i = 0; i < n; ++i) {
             clr[i] = i;
             obr_clr[i].push_back(i);
         }
