c3040e2a10563759f5530beef7e2dbf6a1a8c69c
MatveyOleynikov <ochoba-1@mail.ru>
2025-08-25 00:53:45 +0300

    furie

---
+++ trd/furie.cpp
@@ -0,0 +1,38 @@
+namespace furie {
+    typedef complex<double> base;
+    int rev(int num, int lg_n) {
+        int res = 0;
+        for (int i = 0; i < lg_n; ++i)
+            if (num & (1 << i)) res |= 1 << (lg_n - 1 - i);
+        return res;}
+    void fft(vector<base>& a, bool invert) {
+        const double PI = asin(1) * 2;
+        int n = (int)a.size(); int lg_n = 0;
+        while ((1 << lg_n) < n)  ++lg_n;
+        for (int i = 0; i < n; ++i)
+            if (i < rev(i, lg_n))
+                swap(a[i], a[rev(i, lg_n)]);
+        for (int len = 2; len <= n; len <<= 1) {
+            double ang = 2 * PI / len * (invert ? -1 : 1);
+            base wlen(cos(ang), sin(ang));
+            for (int i = 0; i < n; i += len) {
+                base w(1);
+                for (int j = 0; j < len / 2; ++j) {
+                    base u = a[i + j], v = a[i + j + len / 2] * w;
+                    a[i + j] = u + v;
+                    a[i + j + len / 2] = u - v;
+                    w *= wlen;}}}
+        if (invert)
+            for (int i = 0; i < n; ++i) a[i] /= n;}
+    vi multiply(const vector<int>& a, const vector<int>& b) {
+        vector<base> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+        vector<int> res; size_t n = 1;
+        while (n < max(a.size(), b.size()))  n <<= 1;
+        n <<= 1; fa.resize(n), fb.resize(n);
+        fft(fa, false), fft(fb, false);
+        for (size_t i = 0; i < n; ++i)
+            fa[i] *= fb[i];
+        fft(fa, true); res.resize(n);
+        for (size_t i = 0; i < n; ++i)
+            res[i] = (int)(fa[i].real() + 0.5);
+        return res;}}
